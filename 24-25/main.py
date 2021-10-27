e46541158aefb52508915435868682cb3e2db46e
magdy mandour <m.magdy.mandour@gmail.com>
2021-10-27 23:07:50 +1300

    more tasks added 24-25

---
+++ 24-25/main.py
@@ -0,0 +1,30 @@
+#task 1
+name = "magdy",
+#print(name)
+#print(type(name))
+#end task1
+#task2
+friends = "osama", "magdy", "Hendy"
+list = list(friends)
+list[0] = "Elzero"
+list = tuple(list)
+#print(list)
+#print(type(list))
+#print(len(list))
+#end task2
+#task3
+nums = (1,2,3)
+letters = "a","b","C"
+d = nums + letters
+#print(d)
+#print(len(d))
+#end task3
+#task4
+numbers = (1,2,3,4)
+a,b, _, c = numbers
+print(type(numbers))
+print(numbers[a])
+print(numbers[b])
+#print(numbers[c])
+#I did like how you did in the video but it is giving me out of range !! 
+#task4 
